522fb2f5896b328526bf240945a7a944dc01feac
Shay Stevens <stesh969@student.otago.ac.nz>
2022-05-10 14:56:33 +1200

    Add product domain class
    References #1.

---
+++ etail/src/main/java/domain/Product.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author shaystevens
+ */
+public class Product {
+    private String productID;
+    private String name;
+    private String description;
+    private String category;
+    private BigDecimal listPrice;
+    private BigDecimal quantityInStock;
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public BigDecimal getListPrice() {
+        return listPrice;
+    }
+
+    public void setListPrice(BigDecimal listPrice) {
+        this.listPrice = listPrice;
+    }
+
+    public BigDecimal getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setQuantityInStock(BigDecimal quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+    
+    public String toString(){
+        return "" + productID + ": " + name;
+    }
+}
